b4b109698aecf06a1b21967109c37079b12f4ced
dich2 <112255678+dich2@users.noreply.github.com>
2022-08-27 15:12:03 +0300

    Add files via upload

---
+++ SquareSolver.hpp
@@ -0,0 +1,8 @@
+
+//! Что это такое?
+const int INF = 10;
+
+void sqr_out(int num);
+int is_zero(double x);
+int solve_lin(double k2, double k3, double *x1);
+int solve_sqr(double k1, double k2, double k3, double *x1, double *x2);
---
+++ Tests.hpp
@@ -0,0 +1,3 @@
+
+int ALL_TESTS(void);
+void TEST(double a, double b, double c, int korni);
